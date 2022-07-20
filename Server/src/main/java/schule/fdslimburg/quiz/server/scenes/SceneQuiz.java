1f2a4f16fa4f97c2540341716e97230f5bb9ab1c
lischetzke <6329950+lischetzke@users.noreply.github.com>
2022-07-20 03:10:49 +0200

    Added TODOs

--- Server/src/main/java/schule/fdslimburg/quiz/server/scenes/SceneQuiz.java
+++ Server/src/main/java/schule/fdslimburg/quiz/server/scenes/SceneQuiz.java
@@ -4,6 +4,14 @@ import javafx.scene.Scene;
 import schule.fdslimburg.quiz.server.backend.Backend;
 
 public class SceneQuiz extends AScene {
+	
+	// TODO: Stil und Fragen von WwM?
+	// TODO: Multiple tries allowed
+	// TODO: Tastenkombinationen!
+	//          T -> Start timer after reading question
+	//          1 - 4 -> Answer A - D after buzz
+	//          C -> Close question
+	//          N -> next question
 	@Override
 	public void createScene (Backend backend) {
 	
--- Server/src/main/java/schule/fdslimburg/quiz/server/scenes/SceneScore.java
+++ Server/src/main/java/schule/fdslimburg/quiz/server/scenes/SceneScore.java
@@ -4,6 +4,10 @@ import javafx.scene.Scene;
 import schule.fdslimburg.quiz.server.backend.Backend;
 
 public class SceneScore extends AScene {
+	
+	// TODO: Auto point calculation
+	//          Correct: +<amount of players>
+	//          Wrong: everyone gets one point
 	@Override
 	public void createScene (Backend backend) {
 	
